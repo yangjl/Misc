76c3044a9500b0095d6c9858026741e6fcc011fe
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-05-14 23:20:43 -0700

    send out the report.

--- profiling/4.sweet/4.B.4_GenABEL_step3.R
+++ profiling/4.sweet/4.B.4_GenABEL_step3.R
@@ -19,21 +19,23 @@ res2.mm <- results(res2.mm)
 write.table(res1.mm[, 1:11], "data/Table_gwas_10kw.txt", sep="\t", quote=FALSE)
 write.table(res2.mm[, 1:11], "data/Table_gwas_tkw.txt", sep="\t", quote=FALSE)
 
+pdf("graphs/Figure_results.pdf", width=10, height=5)
 par(mfrow=c(1,2))
-plot(x=res1.mm$Position, y=-log10(res1.mm$Pc1df), xlab="Chromosome 5", main="10 Kernel Weight",
+plot(x=res1.mm$Position, y=-log10(res1.mm$Pc1df), xlab="Chromosome 5", main="Ten Kernel Weight",
      ylab="-log10(q-value)", pch=19, col="cadetblue")
 abline(v=127466000, col="blue", lty=2, lwd=2)
 abline(h=-log10(0.01), col="red", lty=2, lwd=2)
-#S5_128108485
+#S5_128108485 B73=T
 
 plot(x=res2.mm$Position, y=-log10(res2.mm$Pc1df), xlab="Chromosome 5", main="Total Kernel Weight",
      ylab="-log10(q-value)", pch=19, col="cadetblue")
 abline(v=127466000, col="blue", lty=2, lwd=2)
 abline(h=-log10(0.01), col="red", lty=2, lwd=2)
-#S5_127255673
+#S5_127255673 B73=A
+dev.off()
 
 ######### heritability and effect size #################
-geth2 <- function(snpid = "S5_127255673", gm=gm, res1=res1.mm, model=as.formula("TKW")){
+geth2 <- function(snpid = "S5_127255673", gm=gm, res1=res1.mm){
     snp <- as.numeric(gtdata(gm[, snpid]))
     #table(snp,exclude=NULL)
     # introduce missing data indicator
@@ -51,25 +53,34 @@ geth2 <- function(snpid = "S5_127255673", gm=gm, res1=res1.mm, model=as.formula(
     propVarExBySNP1 <- res1[snpid,"chi2.1df"]/res1[snpid,"N"]
     propHerExBySNP1 <- propVarExBySNP1/pol1$esth2
     propVarExBySNP2 <- res1[snpid,"chi2.1df"]/res1[snpid,"N"]
-    propHerExBySNP2 <- propVarExBySNP2/pol1$esth2
+    propHerExBySNP2 <- propVarExBySNP2/pol2$esth2
     res <- data.frame(snp=snpid, trait=c("TKW", "X10KW"), 
-                      varp=c(propVarExBySNP1, propVarExBySNP1), 
-                      varh2=c(propHerExBySNP1, propHerExBySNP1) )
+                      varp=c(propVarExBySNP1, propVarExBySNP2), 
+                      varh2=c(propHerExBySNP1, propHerExBySNP2),
+                      h2=c(pol1$esth2, pol2$esth2))
     return(res)
 }
 
-geth2(snpid = "S5_127255673", gm=gm, res1=res1.mm, model=as.formula("TKW"))
+h21 <- geth2(snpid = "S5_128108485", gm=gm, res1=res1.mm)
+h21 <- h21[-1,]
 
+h22 <- geth2(snpid = "S5_127255673", gm=gm, res1=res2.mm)
+h22 <- h22[-2,]
+plotbox_1 <- function(){
+    snp <- as.numeric(gtdata(gm[, "S5_127255673"]))
+    pheno <- phdata(gm)
+    df <- merge(pheno, snp, by="row.names")
+    df <- subset(df, !is.na(S5_127255673))
+    boxplot(TKW ~ as.factor(S5_127255673), data=df, names=c("B73-like","non-B73"))
+    print(t.test(subset(df, S5_127255673==0)$TKW, subset(df, S5_127255673==2)$TKW))
+}
+plotbox_1()
 
-
-
-
-propVarExBySNP <- res2.mm["S5_127255673","chi2.1df"]/res2.mm["S5_127255673","N"]
-propHerExBySNP <- propVarExBySNP/h2a1$esth2
-
-pheno <- phdata(gm)
-df <- merge(pheno, snp, by="row.names")
-df <- subset(df, !is.na(S5_127255673))
-boxplot(TKW ~ as.factor(S5_127255673), data=df)
-
-t.test(subset(df, S5_127255673==0)$TKW, subset(df, S5_127255673==2)$TKW)
+plotbox_2 <- function(){
+    snp <- as.numeric(gtdata(gm[, "S5_128108485"]))
+    pheno <- phdata(gm)
+    df <- merge(pheno, snp, by="row.names")
+    df <- subset(df, !is.na(S5_128108485))
+    boxplot(X10KW ~ as.factor(S5_128108485), data=df, names=c("B73-like","non-B73"))
+}
+plotbox_2()
