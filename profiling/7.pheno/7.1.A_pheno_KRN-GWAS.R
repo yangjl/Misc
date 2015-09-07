2cb449006ba6672edf5df0a8ab846d2e0522cd96
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-09-07 16:19:08 -0700

    more edits

--- profiling/7.pheno/7.1.A_pheno_KRN-GWAS.R
+++ profiling/7.pheno/7.1.A_pheno_KRN-GWAS.R
@@ -6,10 +6,10 @@ namlong$KRN <- as.numeric(namlong$KRN)
 namlong <- namlong[!is.na(namlong$KRN),]
 
 library("lme4")
-m1 <- lmer(KRN ~ Genotype + block + plot + (1 | Year) + (1 | Location), data=namlong)
+m1 <- lmer(KRN ~ Genotype + (1 |block) + (1 |plot) + (1 |Year) + (1 |Location), data=namlong)
+m2 <- lmer(KRN ~ Genotype + (1 |block) + (1 |plot) + (1 |Year) + (1 |Location) + (1 |Genotype:Year) + (1 |Genotype:Location), data=namlong)
 
+m3 <- lmer(KRN ~ Genotype + (1 |Year) + (1 | Location), data=namlong)
+m4 <- lmer(KRN ~ Genotype + (1 |Year) + (1 | Location) + (1 |Genotype:Year) + (1 |Genotype:Location), data=namlong)
 
-m2 <- lmer(KRN ~ Genotype + (1 | Year) + (1 | Location) + (1 | Genotype:Year) + (1 | Genotype:Location), data=namlong)
-
-
-save(file="cache/krn_lmer.RData", list=c("m1", "m2"))
+save(file="cache/krn_lmer.RData", list=c("m1", "m2", "m3", "m4"))
