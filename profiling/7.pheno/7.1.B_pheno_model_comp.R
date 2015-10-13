fa0740bfc2434227f4aa0410091e3d6469a39af9
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-10-12 17:32:06 -0700

    hw for hyl

--- profiling/7.pheno/7.1.B_pheno_model_comp.R
+++ profiling/7.pheno/7.1.B_pheno_model_comp.R
@@ -12,4 +12,21 @@ res2 <- anova(m3, m4)
 
 save(file="cache/krn_models.RData", list=c("res1", "res2"))
 
-     
+ob2 <- load("cache/krn_models.RData")
+
+
+Genotype:Location (Intercept) 12.643  
+Genotype:Year     (Intercept)  9.503  
+plot              (Intercept) 11.521  
+block             (Intercept)  0.000  
+Location          (Intercept)  2.939  
+Year              (Intercept) 10.477  
+Residual                      32.055  
+
+Groups   Name        Std.Dev.
+plot     (Intercept) 16.611  
+block    (Intercept)  1.929  
+Location (Intercept)  1.595  
+Year     (Intercept) 10.147  
+Residual             32.121
+
