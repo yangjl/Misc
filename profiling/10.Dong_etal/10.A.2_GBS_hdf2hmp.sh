257ca76090a50aff8cc95284d0a6e8145b570ee6
yangjl0930@gmail.com <yangjl0930@gmail.com>
2017-01-23 00:04:52 -0800

    updates

---
+++ profiling/10.Dong_etal/10.A.2_GBS_hdf2hmp.sh
@@ -0,0 +1,12 @@
+
+# load modules
+module load gcc jdk/1.8 tassel/5
+# to export hdf5 file to Hapmap format
+run_pipeline.pl -Xmx64g -fork1 -h5 ZeaGBSv27_publicSamples_imputedV5_AGPv2-150114.h5 \\
+-export -exportType Hapmap -runfork1
+
+# sort the GBS2.7 - this takes a while (1 hour)
+run_pipeline.pl -Xmx64g -SortGenotypeFilePlugin -inputFile AllZeaGBSv2.7_publicSamples_imputedV3b_agpv3.hmp.gz -outputFile ZeaGBSv27_agpv3_sorted -fileType Hapmap
+
+# for GBS2.7, the "keep_list_NAM_children.txt" is just a one column list to keep
+run_pipeline.pl -Xmx64g -fork1 -h ZeaGBSv27_agpv3_sorted.hmp.txt -includeTaxaInfile ~/Documents/Github/Misc/data/Taxa_ames282_288.txt -export ZeaGBSv27_Ames282_agpv3 -exportType Hapmap -runfork1
