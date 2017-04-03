95a6ba091b9d72f3c0b67c29267ce2cbbac54f1e
yangjl0930@gmail.com <yangjl0930@gmail.com>
2017-04-03 15:28:13 -0700

    edits according to Eric's suggestions.

--- profiling/10.Dong_etal/10.D.7_hufford2012_ms_param.R
+++ profiling/10.Dong_etal/10.D.7_hufford2012_ms_param.R
@@ -5,12 +5,22 @@ library(data.table)
 library(tidyr)
 library(ggplot2)
 #AGPv2:GRMZM2G039867, chr3:150,049,274..150,052,250
-
+# AGPv4: 151329451 151332389 + BP
 d <- fread("largedata/Hufford2012/757736/Hufford_et_al._2012_10kb_statistics.txt", data.table=FALSE)
-
+fsize=12
 
 BP = 50000
 subd <- subset(d, chr==3 & winstart > 150049274 -BP & winend < 150052250 + BP)
+
+#bed <- subd[, c(1, 9, 10)]
+#bed$uid <- paste(bed[,1], bed[,2], sep="_")
+#write.table(bed, "data/chr3_v2.bed", sep="\t", row.names=FALSE, quote=FALSE)
+
+#nbed <- read.table("data/output_chr3_v2.bed")
+#nbed <- nbed[!duplicated(nbed$V4),]
+
+v24 <- 151329451 - 150049274
+
 tem <- subd[, c("chr", "winstart", "winend", "ThetaPiMZ", "ThetaPiLR", "ThetaPiteo")]
 out <- gather(tem, pop, pi, 4:6)
 out <- subset(out, !is.na(pi))
@@ -18,6 +28,8 @@ out <- subset(out, !is.na(pi))
 out[out$pop == "ThetaPiMZ", ]$pop <- "Maize"
 out[out$pop == "ThetaPiteo", ]$pop <- "Teosinte"
 out[out$pop == "ThetaPiLR", ]$pop <- "Landrace"
+
+out$winstart <- out$winstart + v24
 
 p2 <- ggplot(out, aes(x=winstart, y=pi, colour=factor(pop, levels=c("Maize", "Landrace", "Teosinte")) )) +
     labs(colour="") +
@@ -31,14 +43,15 @@ p2 <- ggplot(out, aes(x=winstart, y=pi, colour=factor(pop, levels=c("Maize", "La
     geom_hline(yintercept = mean(d$ThetaPiMZ, na.rm=T), col="#458b74") +
     geom_hline(yintercept = mean(d$ThetaPiteo, na.rm=T), col="#8b2323") +
     geom_hline(yintercept = mean(d$ThetaPiLR, na.rm=T), col="#8b2323") +
-    geom_vline(xintercept = c(150049274, 150052250), col="grey") +
+    geom_vline(xintercept = c(150049274, 150052250) +v24, col="grey") +
     theme(axis.text.y = element_text(angle = 90, hjust = 0.5),
           axis.text=element_text(size=fsize),
           axis.title=element_text(size=fsize, face="bold"),
           legend.title = element_text(size=fsize, face="bold"),
-          legend.text = element_text(size=fsize),
-          axis.text.x=element_blank(),
-          axis.ticks.x=element_blank())
+          legend.text = element_text(size=fsize) )
+          #axis.text.x= element_blank() ) +
+          #axis.ticks.x=element_blank())
+    
 
 pdf("graphs/Fig4.pdf", height=4, width=6)
 p2
@@ -77,6 +90,7 @@ out[out$pop == "TajDMZ", ]$pop <- "Maize"
 out[out$pop == "TajDLR", ]$pop <- "Landrace"
 out[out$pop == "TajDteo", ]$pop <- "Teosinte"
 
+out$winstart <- out$winstart + v24
 
 p1 <- ggplot(out, aes(x=winstart, y=tajd, colour=factor(pop, levels=c("Maize", "Landrace", "Teosinte")) )) +
     labs(colour="") +
@@ -90,14 +104,12 @@ p1 <- ggplot(out, aes(x=winstart, y=tajd, colour=factor(pop, levels=c("Maize", "
     geom_hline(yintercept = mean(d$TajDMZ, na.rm=T), col="#458b74") +
     geom_hline(yintercept = mean(d$TajDLR, na.rm=T), col="#6495ed") +
     geom_hline(yintercept = mean(d$TajDteo, na.rm=T), col="#8b2323") +
-    geom_vline(xintercept = c(150049274, 150052250), col="grey") +
+    geom_vline(xintercept = c(150049274, 150052250) +v24, col="grey") +
     theme(axis.text.y = element_text(angle = 90, hjust = 0.5),
           axis.text=element_text(size=fsize),
           axis.title=element_text(size=fsize, face="bold"),
           legend.title = element_text(size=fsize, face="bold"),
-          legend.text = element_text(size=fsize),
-          axis.text.x=element_blank(),
-          axis.ticks.x=element_blank())
+          legend.text = element_text(size=fsize))
 p1
 
 
